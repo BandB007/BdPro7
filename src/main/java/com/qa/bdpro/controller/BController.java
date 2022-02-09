b66141c955df09ae72659da1f579986f9bb7245a
BanndB007 <bdstudy07@gmail.com>
2022-02-09 23:38:23 +0000

    Construc inject

--- src/main/java/com/qa/bdpro/controller/BController.java
+++ src/main/java/com/qa/bdpro/controller/BController.java
@@ -1,5 +1,14 @@
 package com.qa.bdpro.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BController {
+	private BService service;
+	
+	//Constructor injection
+		public BController (BService service) {
+			this.service = service;
+		}
 
 }
