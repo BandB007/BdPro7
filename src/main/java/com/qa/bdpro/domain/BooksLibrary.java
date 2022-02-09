6f84bc7996aa754c88a1b0c22124fb2f241a4ce0
BanndB007 <bdstudy07@gmail.com>
2022-02-09 22:59:55 +0000

    Domain Created

--- src/main/java/com/qa/bdpro/domain/BooksLibrary.java
+++ src/main/java/com/qa/bdpro/domain/BooksLibrary.java
@@ -1,5 +1,30 @@
 package com.qa.bdpro.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Books")
 public class BooksLibrary {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column
+	private Long id;
+	
+	@Column (nullable = false)
+	private String title;
+	
+	@Column (nullable = false)
+	private String author;
+	
+	@Column 
+	private String available;
+
+	@Column
+	private String reserved;
 
 }
