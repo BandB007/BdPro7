1e3ab7b273339679aec2133e0ea04a17e0879741
BanndB007 <bdstudy07@gmail.com>
2022-02-09 23:56:14 +0000

    ignore prev commit this one with CRUD

--- src/main/java/com/qa/bdpro/controller/BController.java
+++ src/main/java/com/qa/bdpro/controller/BController.java
@@ -1,6 +1,18 @@
 package com.qa.bdpro.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.bdpro.domain.BooksLibrary;
 
 @RestController
 public class BController {
@@ -10,5 +22,35 @@ public class BController {
 		public BController (BService service) {
 			this.service = service;
 		}
+		
+	//Create
+		@PostMapping("/create")
+		public ResponseEntity<BooksLibrary>create(@RequestBody BooksLibrary book) {
+			return new ResponseEntity<BooksLibrary>(this.service.create(book),HttpStatus.CREATED);
+		}
+		
+	//read
+		@GetMapping("/getall")
+		public ResponseEntity<List<BooksLibrary>> getAll() {
+			return new ResponseEntity<List<BooksLibrary>>(this.service.getAll(), HttpStatus.OK);
+		}
+		
+	//finding by id
+		@GetMapping("/getOne/{id}")
+		public ResponseEntity<BooksLibrary>getOne(@PathVariable Long id) {
+			return new ResponseEntity<BooksLibrary>(this.service.readbyid(id), HttpStatus.ACCEPTED);
+		}
+	
+	//update
+		@PutMapping("/replace/{id}")
+		public ResponseEntity<BooksLibrary>replace(@PathVariable Long id, BooksLibrary book) {
+			return new ResponseEntity<BooksLibrary>(this.service.getOne(id), HttpStatus.ACCEPTED);
+		}
 
+	//delete
+		@DeleteMapping("/delete/{id}")
+		public ResponseEntity<BooksLibrary>delete(@PathVariable Long id, BooksLibrary book) {
+			return new ResponseEntity<BooksLibrary>(this.service.getOne(id), HttpStatus.OK);
+
+		}
 }
