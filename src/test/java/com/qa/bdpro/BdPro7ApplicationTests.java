caea00c9dfcc50777de7904a6bf9b20c80a4d579
BanndB007 <bdstudy07@gmail.com>
2022-02-09 22:40:26 +0000

    Initial commit

---
+++ src/test/java/com/qa/bdpro/BdPro7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.bdpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdPro7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
