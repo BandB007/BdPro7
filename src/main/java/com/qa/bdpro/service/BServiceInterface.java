72a64ac769a14bd4a9699bdca1e4a89bc978ae23
BanndB007 <bdstudy07@gmail.com>
2022-02-10 13:38:43 +0000

    service interface updated

--- src/main/java/com/qa/bdpro/service/BServiceInterface.java
+++ src/main/java/com/qa/bdpro/service/BServiceInterface.java
@@ -1,5 +1,21 @@
 package com.qa.bdpro.service;
 
-public interface BServiceInterface {
+import java.util.List;
 
+public interface BServiceInterface <T> {
+
+	T create(T book);
+
+	List<T> readAll();
+	
+	T readone(Long id);
+
+	T update(Long id, T book);
+
+	T delete(Long id);
+
+	T readOne(Long id);
 }
+
+
+
