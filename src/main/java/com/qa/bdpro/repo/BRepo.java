15a73b9e4ccfa1f668ea202d0a6bb1315cd79578
BanndB007 <bdstudy07@gmail.com>
2022-02-10 11:11:30 +0000

    domain and dependency imported

--- src/main/java/com/qa/bdpro/repo/BRepo.java
+++ src/main/java/com/qa/bdpro/repo/BRepo.java
@@ -1,8 +1,11 @@
 package com.qa.bdpro.repo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.qa.bdpro.domain.BooksLibrary;
+
 @Repository
-public interface BRepo {
+public interface BRepo extends JpaRepository<BooksLibrary, Long>{
 
 }
